f7d02b1bfee6c1705cd77bb935e1fd5c2960fbef
GlennJo <g.j@live.se>
2024-06-05 09:41:46 +0200

    STATIC_ROOT = BASE_DIR / 'static_files'
    This was corrected

--- shop/settings.py
+++ shop/settings.py
@@ -113,7 +113,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static_files'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
