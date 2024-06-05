12294bc59a988c46a5ed9257dcae432af8f1be0a
GlennJo <g.j@live.se>
2024-06-05 11:54:56 +0200

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
         },
     },
 ]
+
 WSGI_APPLICATION = 'shop.wsgi.application'
 AUTH_USER_MODEL = 'accounts.Account'
 
@@ -73,24 +74,24 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-#else:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
